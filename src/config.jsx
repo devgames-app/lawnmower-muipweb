102ed6d2b687e458b351a4f45ff4d91d1fec7975
Gustavo Kei Amemiya <gustavokeia@gmail.com>
2023-04-16 19:48:08 -0300

    chore: fixing typo on config.jsx and renaming .env.local.example

--- src/config.jsx
+++ src/config.jsx
@@ -2,6 +2,6 @@ export default {
   muip: {
     host: import.meta.env.VITE_MUIP_HOST,
     port: import.meta.env.VITE_MUIP_PORT,
-    region: import.meta.env.MUVITE_IP_REGION,
+    region: import.meta.env.VITE_MUIP_REGION,
   },
 };
